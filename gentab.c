669056f5816774c4cffff7b11169571e1a897558
ViStefan <1586641+ViStefan@users.noreply.github.com>
2025-08-13 03:00:40 +0300

    extract functions, pretty-print chunks

--- gentab.c
+++ gentab.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 
-#define WIDTH 3
-#define HEIGHT 3
+#define WIDTH 4
+#define HEIGHT 4 
+
+#define CHUNK_SIZE (WIDTH - 2) * (HEIGHT - 2)
 
 int MAP[] = {
     //0, 8, 9, 1,
     //14, 4, 5, 10,
     //15, 7, 6, 11,
     //3, 13, 12, 2
-    0, 1, 2,
-    3, 4, 5,
-    6, 7, 8,
+    //0, 5, 3,
+    //6, 4, 7,
+    //2, 4, 1,
+    //0, 1, 2, 3,
+    //4, 5, 6, 7,
+    //8, 9, 10, 11,
+    //12, 13, 14, 15
+    0, 1, 2, 3,
+    11, 12, 13, 4,
+    10, 15, 14, 5,
+    9, 8, 7, 6
 };
 
 int getbit(int bits, int x, int y)
@@ -37,6 +47,20 @@ int life(int bits, int w, int h)
     return(neighbours == 3 || (self && neighbours == 2));
 }
 
+int life_chunk(int bits)
+{	
+    int out = 0;
+
+    for (int h = 1; h < HEIGHT - 1; ++h)
+    {
+        for (int w = 1; w < WIDTH - 1; ++w)
+        {
+            out = (out << 1) | life(bits, w, h);
+        }
+    }
+    return out;
+}
+
 void putsquare(int color)
 {
     if (color)
@@ -45,34 +69,54 @@ void putsquare(int color)
         printf("\u2b1c");
 }
 
+void pretty_print(int in, int out)
+{
+    for (int h = 0; h < HEIGHT; ++h)
+    {
+        for (int w = 0; w < WIDTH; ++w)
+        {
+            putsquare(getbit(in, w, h));
+        }
+
+        if (h > 0 && h < HEIGHT - 1)
+        {
+            if (h == (HEIGHT - 1) / 2)
+                printf(" → ");
+            else
+                printf("   ");
+            
+            for (int w = 0; w < WIDTH - 2; ++w)
+            {
+                putsquare((out >> ((CHUNK_SIZE - 1) - (w + (h - 1) * (WIDTH - 2)))) & 1);
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
 int main(int argc, char **argv)
 {
 	long int size = 1 << (WIDTH * HEIGHT);
+    int result[size];
 	for (int i = 0; i < size; ++i)
 	{
 		printf("\n%d:\n", i);
-
-		for (int h = 0; h < HEIGHT; ++h)
-        {
-			for (int w = 0; w < WIDTH; ++w)
-			{
-                putsquare(getbit(i, w, h));
-            }
-
-            if (h > 0 && h < HEIGHT - 1)
-            {
-                if (h == (HEIGHT - 1) / 2)
-                    printf(" → ");
-                else
-                    printf("   ");
-                
-                for (int w = 1; w < WIDTH - 1; ++w)
-                {
-                    putsquare(life(i, w, h));
-                }
-            }
-
-            printf("\n");
-        }
+        result[i] = life_chunk(i);
+        pretty_print(i, result[i]);
 	}
+
+    int prev = result[0];
+    int n = 1;
+    printf("0");
+    for (int i = 1; i < size; ++i)
+    {
+        if (result[i] != prev)
+        {
+            printf(", %d", i);
+            prev = result[i];
+            ++n;
+        }
+    }
+    printf("\n--%d--", n);
 }
