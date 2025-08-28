cc7f3565c64eb8d1be3b91a0fbb1aad411ed907f
ViStefan <1586641+ViStefan@users.noreply.github.com>
2025-08-28 17:28:35 +0300

    revert slow threading for each cell

--- life.c
+++ life.c
@@ -1,22 +1,5 @@
-#include <pthread.h>
 #include "map.h"
 #include "life.h"
-
-struct thread_data
-{
-    int bits;
-    int width;
-    int height;
-    map *m;
-    int *result;
-} typedef thread_data;
-
-void *life_thread(void *t)
-{
-    thread_data *td = ((thread_data*) t);
-    *(td->result) = life(td->bits, td->width, td->height, td->m);
-    return NULL;
-}
 
 int life(int bits, int w, int h, map *m)
 {
@@ -38,34 +21,14 @@ int life(int bits, int w, int h, map *m)
 
 int life_chunk(int bits, map *m)
 {	
-    int size = (m->width - 2) * (m->height - 2);
-    pthread_t pthreads[size];
-    int result[size];
-    thread_data td[size];
-
-    int i = 0;
+    int out = 0;
 
     for (int h = 1; h < m->height - 1; ++h)
     {
         for (int w = 1; w < m->width - 1; ++w)
         {
-            td[i].bits = bits;
-            td[i].width = w;
-            td[i].height = h;
-            td[i].m = m;
-            td[i].result = &result[i];
-            pthread_create(&pthreads[i], NULL, life_thread, &td[i]);
-            ++i;
+            out = (out << 1) | life(bits, w, h, m);
         }
-    }
-
-    for (i = 0; i < size; ++i)
-        pthread_join(pthreads[i], NULL);
-
-    int out = 0;
-    for (i = 0; i < size; ++i)
-    {
-        out = (out << 1) | result[i];
     }
     return out;
 }
