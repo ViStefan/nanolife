1f48e3aa9b17deebb2f1b42be5914eb3da16b4df
ViStefan <1586641+ViStefan@users.noreply.github.com>
2025-09-08 01:00:30 +0300

    improvement for tests

--- test/smoke.sh
+++ test/smoke.sh
@@ -30,19 +30,26 @@ assert() {
     fi
 }
 
+TMP_FILE=test/$(date --iso-8601=seconds).tmp
+trap "rm -rf -- $TMP_FILE" EXIT
+
 echo Permutations number
-assert "$(./permutate 9 | wc -l)" "362880"
+./permutate 9 > $TMP_FILE
+assert "$(wc -l --total=only $TMP_FILE)" "362880"
 
 echo Permutations uniqness 
-assert "$(./permutate 9 | sort -h | uniq -d)" ""
+assert "$(sort -h $TMP_FILE | uniq -d)" ""
 
 echo Bruteforce number
-assert "$(./brute_4x4 | wc -l)" "362880"
+./brute_4x4 > $TMP_FILE
+assert "$(wc -l --total=only $TMP_FILE)" "362880"
 
 echo Bruteforce uniqness 
-assert "$(./brute_4x4 | sort -h | uniq -d)" ""
+assert "$(sort -h $TMP_FILE | uniq -d)" ""
 
 echo Generate lookup table
+assert "$(./gentab outcome 3x3 1,2,3,4,0,5,6,7,8 pretty 0 | sed -n 's/^monotonous outcome intervals: \(.*\)$/\1/p')" "97"
+assert "$(./gentab outcome 3x4 default pretty 0 | sed -n 's/^monotonous outcome intervals: \(.*\)$/\1/p')" "1332"
 assert "$(./gentab outcome 4x4 1,0,3,2,5,4,7,6,9,8,11,10,13,12,15,15 pretty 0 | sed -n 's/^monotonous outcome intervals: \(.*\)$/\1/p')" "35617"
 
 echo
