153f5da4b48da25193353cb9891f4cc90aff245d
ViStefan <1586641+ViStefan@users.noreply.github.com>
2025-09-13 18:22:52 +0300

    satisfy shellcheck in test script, add results to readme

--- test/smoke.sh
+++ test/smoke.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 FAILED=0
 
@@ -7,54 +7,53 @@ color() {
     local color=${esc}$1m
     local end=${esc}0m
 
-    printf "%s$2%s" $color $end
+    printf "%s$2%s" "$color" "$end"
 }
 
 fail() {
-    echo -e $(color 31 Failed) "${@}" 
-    exit 1
+    echo -e "$(color 31 Failed)" "${@}"
 }
 
 ok() {
-    echo -e $(color 32 Ok) "${@}"  
-    exit 0
+    echo -e "$(color 32 Ok)" "${@}"
 }
 
 
 assert() {
     if [ "$1" = "$2" ]; then
-        echo $(ok $1)
+        ok "$1"
     else
         export FAILED=1
-        echo $(fail $1 != $2)
+        fail "$1" != "$2"
     fi
 }
 
 TMP_FILE=test/$(date --iso-8601=seconds).tmp
-trap "rm -rf -- $TMP_FILE" EXIT
+trap 'rm -rf -- $TMP_FILE' EXIT
 
 echo Permutations number
-./permutate 9 > $TMP_FILE
-assert "$(wc -l --total=only $TMP_FILE)" "362880"
+./permutate 9 > "$TMP_FILE"
+assert "$(wc -l --total=only "$TMP_FILE")" "362880"
 
 echo Permutations uniqness 
-assert "$(sort -h $TMP_FILE | uniq -d)" ""
+assert "$(sort -h "$TMP_FILE" | uniq -d)" ""
 
 echo Bruteforce number
-./brute_4x4 3x3 $(nproc) ALL > $TMP_FILE
-assert "$(wc -l --total=only $TMP_FILE)" "362880"
+./brute_4x4 3x3 "$(nproc)" ALL > "$TMP_FILE"
+assert "$(wc -l --total=only "$TMP_FILE")" "362880"
 
 echo Bruteforce uniqness 
-assert "$(sort -h $TMP_FILE | uniq -d)" ""
+assert "$(sort -h "$TMP_FILE" | uniq -d)" ""
 
 echo Bruteforce result
-./brute_4x4 3x3 $(nproc) MIN > $TMP_FILE
-assert "$(tail -n 1 $TMP_FILE | cut -d' ' -f1)" "97"
+./brute_4x4 3x3 "$(nproc)" MIN > "$TMP_FILE"
+assert "$(tail -n 1 "$TMP_FILE" | cut -d' ' -f1)" "97"
 
 echo Generate lookup table
-assert "$(./gentab OUTCOME 3x3 1,2,3,4,0,5,6,7,8 PRETTY 0 $(nproc) | sed -n 's/^monotonous outcome intervals: \(.*\)$/\1/p')" "97"
-assert "$(./gentab OUTCOME 3x4 DEFAULT PRETTY 0 $(nproc) | sed -n 's/^monotonous outcome intervals: \(.*\)$/\1/p')" "1332"
-assert "$(./gentab OUTCOME 4x4 1,0,3,2,5,4,7,6,9,8,11,10,13,12,15,15 PRETTY 0 $(nproc) | sed -n 's/^monotonous outcome intervals: \(.*\)$/\1/p')" "35617"
+assert "$(./gentab OUTCOME 3x3 1,2,3,4,0,5,6,7,8 PRETTY 0 "$(nproc)" | sed -n 's/^monotonous outcome intervals: \(.*\)$/\1/p')" "97"
+assert "$(./gentab OUTCOME 3x4 DEFAULT PRETTY 0 "$(nproc)" | sed -n 's/^monotonous outcome intervals: \(.*\)$/\1/p')" "1332"
+assert "$(./gentab OUTCOME 4x4 1,0,3,2,5,4,7,6,9,8,11,10,13,12,15,15 PRETTY 0 "$(nproc)" | sed -n 's/^monotonous outcome intervals: \(.*\)$/\1/p')" "35617"
 
 echo
-[ $FAILED -eq 0 ] && ok "No errors occurred" || fail "Some errors occured"
+[ $FAILED -eq 0 ] && ok "No errors occurred" || fail "Some errors occurred"
+exit $FAILED
